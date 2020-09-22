a080517ef5e6373030d45ec1637f7cddfc5e7e44
hieuducnguyen <ndhieu.bk@gmail.com>
2020-09-22 16:29:27 +0700

    Test branch3 (#2)
    * add branch 3
    
    * resolve conflict

--- src/gitlear/TestMaster.java
+++ src/gitlear/TestMaster.java
@@ -11,6 +11,13 @@ package gitlear;
  */
 public class TestMaster {
 
+	public static void main(String[] args) {
+		testBranch3();
+	}
+
+	private static void testBranch3() {
+		System.out.println("testBranch3");
+	}
 	void addFuncInMaster() {
 		System.out.println("addFuncInMaster");
 	}
