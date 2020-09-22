76784f93292114e85ed65317b4462ea8503586ce
HieuNguyen <ndhieu.bk@gmail.com>
2020-09-22 14:03:26 +0700

    add b1

--- src/gitlear/GItLear.java
+++ src/gitlear/GItLear.java
@@ -15,7 +15,7 @@ public class GItLear {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		System.out.println("git b 1");
 	}
 	
 }
